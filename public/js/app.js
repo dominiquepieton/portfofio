ac61bdf2d100d2c1b553958eba0fdabbce78006a
dominiquepieton <pietondominique@yahoo.fr>
2020-07-07 12:20:02 +0200

    commit init

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+// Animation écriture
+const txtAnim = document.querySelector('.txt-animation');
+let typewriter = new Typewriter(txtAnim, {
+    loop: false,
+    deleteSpeed: 20
+});
+
+typewriter.pauseFor(1800)
+        .changeDelay(20)
+        .typeString('Je suis Dominique PIETON')
+        .pauseFor(300)
+        .typeString('<strong>, Développeur Full - Stack </strong> !')
+        .pauseFor(1000)
+        .deleteChars(15)
+        .typeString('<span style="color: #27ae60;"> CSS</span> !')
+        .pauseFor(1000)
+        .deleteChars(5)
+        .typeString('<span style="color: #ff6910;"> JAVASCRIPT</span> !')
+        .pauseFor(1000)
+        .deleteChars(12)
+        .typeString('<span style="color: #EA39ff;"> PHP/SYMFONY</span> !')
+        .start();
