22ee3874ece23e0783732404d0c336558d716c96
dominiquepieton <pietondominique@yahoo.fr>
2020-07-18 15:45:44 +0200

    modification de la home page

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ AOS.init({
 // Animation écriture
 const txtAnim = document.querySelector('.txt-animation');
 let typewriter = new Typewriter(txtAnim, {
-    loop: false,
+    loop: true,
     deleteSpeed: 20
 });
 
@@ -16,16 +16,16 @@ typewriter.pauseFor(1800)
         .changeDelay(20)
         .typeString('Je suis Dominique PIETON')
         .pauseFor(300)
-        .typeString('<strong>, Développeur Full - Stack </strong> !')
+        .typeString('<strong>, Développeur  Full - Stack </strong> !')
         .pauseFor(1000)
         .deleteChars(15)
-        .typeString('<span style="color: #27ae60;"> CSS</span> !')
+        .typeString('<span style="color: #27ae60;">  CSS</span> !')
         .pauseFor(1000)
         .deleteChars(5)
-        .typeString('<span style="color: #ff6910;"> JAVASCRIPT</span> !')
+        .typeString('<span style="color: #ff6910;">  JAVASCRIPT</span> !')
         .pauseFor(1000)
         .deleteChars(12)
-        .typeString('<span style="color: #EA39ff;"> PHP/SYMFONY</span> !')
+        .typeString('<span style="color: #EA39ff;">  PHP/SYMFONY</span> !')
         .start();
 
 /**** retour vers le haut ******/
